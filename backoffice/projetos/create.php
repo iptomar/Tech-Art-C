2f4f2aa35d443e1c3592a9c3a84ed1f29e707756
Bernardo Oliveira <aluno23025@ipt.pt>
2024-04-04 19:32:44 +0100

    Adição de gestores
    Foi adicionado gestores para que possam ser escolhidos pelos investigadores e gestores também, devido a serem criadores do projeto em causa.
    
    Co-Authored-By: DiniSilva <101589841+DiniSilva@users.noreply.github.com>

--- backoffice/projetos/create.php
+++ backoffice/projetos/create.php
@@ -24,6 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $financiamento = $_POST["financiamento"];
     $ambito = $_POST["ambito"];
     $investigadores = [];
+    $gestores = [];
     $concluido = isset($_POST['concluido']) ? 1 : 0;
     $site = $_POST["site"];
     $facebook = $_POST["facebook"];
@@ -40,23 +41,38 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST["investigadores"])) {
         $investigadores = $_POST["investigadores"];
     }
+    if (isset($_POST["gestores"])) {
+        $gestores = $_POST["gestores"];
+    }
     if (mysqli_stmt_execute($stmt)) {
-        if (count($investigadores) == 0) {
-            header('Location: index.php');
-            return;
+        if (count($investigadores) > 0) {
+            $sqlinsert = "";
+            foreach ($investigadores as $id) {
+                $sqlinsert = $sqlinsert . "($id,last_insert_id()),";
+            }
+            $sqlinsert = rtrim($sqlinsert, ",");
+            $sql = "INSERT INTO investigadores_projetos (investigadores_id,projetos_id) VALUES" . $sqlinsert;
+            if (!mysqli_query($conn, $sql)) {
+                echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+                exit;
+            }
+        }   
+
+        if (count($gestores) > 0) {
+            $sqlinsert2 = "";
+            foreach ($gestores as $id2) {
+                $sqlinsert2 = "($id2,last_insert_id()),";
+            }
+            $sqlinsert2 = rtrim($sqlinsert2, ",");
+            $sql2 = "INSERT INTO gestores_projetos (gestor_id,projetos_id) VALUES" . $sqlinsert2;
+            if (!mysqli_query($conn, $sql2)) {
+                echo "Error: " . $sql2 . "<br>" . mysqli_error($conn);
+                exit;
+            }
         }
-        $sqlinsert = "";
-        foreach ($investigadores as $id) {
-            $sqlinsert = $sqlinsert . "($id,last_insert_id()),";
-        }
-        $sqlinsert = rtrim($sqlinsert, ",");
-        $sql = "INSERT INTO investigadores_projetos (investigadores_id,projetos_id) values" . $sqlinsert;
-        if (mysqli_query($conn, $sql)) {
-            header('Location: index.php');
-        } else {
-            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-        }
+        header('Location: index.php');
         exit;
+
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
@@ -319,14 +335,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     </div>
                     <div class="col">
                         <div class="form-group">
-                            <label>Investigadores/as</label><br>
+                            <label>Gestores/as</label><br>
                             <?php
                             $sql = "SELECT id, nome, tipo FROM investigadores 
                                 ORDER BY CASE WHEN tipo = 'Externo' THEN 1 ELSE 0 END, tipo, nome;";
                             $result = mysqli_query($conn, $sql);
                             if (mysqli_num_rows($result) > 0) {
                                 while ($row = mysqli_fetch_assoc($result)) { ?>
-                                    <input type="checkbox" name="investigadores[]" value="<?= $row["id"] ?>">
+                                    <input type="checkbox" name="gestores[]" value="<?= $row["id"] ?>">
                                     <label><?= $row["tipo"] . " - " .  $row["nome"] ?></label><br>
                             <?php }
                             } ?>
