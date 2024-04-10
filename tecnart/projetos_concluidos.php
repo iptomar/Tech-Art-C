afaebf1b9311d5c8642572f39e73babe493bbfd3
goncalo-m-ferreira <103708923+goncalo-m-ferreira@users.noreply.github.com>
2024-04-10 23:44:25 +0100

    Formatar Botões

--- tecnart/projetos_concluidos.php
+++ tecnart/projetos_concluidos.php
@@ -26,6 +26,20 @@ $projetos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <?= template_header(change_lang("projects-finished-page-heading")); ?>
 
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<style type="text/css">
+    <?php
+    $css = file_get_contents('../styleBackoffices.css');
+    echo $css;
+    ?>
+</style>
+
 <!-- product section -->
 <section class="product_section layout_padding">
    <div style="background-color: #dbdee1; padding-top: 50px; padding-bottom: 50px;">
@@ -47,43 +61,41 @@ $projetos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <!-- end product section -->
 
 <section class="product_section layout_padding">
-   <div style="padding-top: 20px;">
-      <div class="container">
-         <!-- Search Bar -->
-         <form method="GET">
-            <div class="row justify-content-center">
-               <div class="col-md-6 mb-3">
-                  <div class="input-group">
-                     <input type="text" class="form-control" placeholder="Search projects..." name="search" value="<?= $search_query ?>">
-                     <div class="input-group-append">
-                        <button class="btn btn-outline-secondary" type="submit">Search</button>
-                     </div>
-                  </div>
+   <!-- Search Bar -->
+   <form method="GET" action="projetos_concluidos">"
+      <div class="row justify-content-center">
+         <div class="col-md-6 mb-3">
+            <div class="input-group mb-3">
+               <input type="text" name="search" class="form-control" placeholder="Search projects..." id="searchInput">
+               <div class="input-group-append">
+                  <button class="btn btn-outline-secondary" type="submit" id="searchButton"><i class="fa fa-search"></i></button>
                </div>
             </div>
-         </form>
-         <!-- End of Search Bar -->
-
-         <div class="row justify-content-center mt-3">
-
-            <?php foreach ($projetos as $projeto) : ?>
-
-               <div class="ml-5 imgList">
-                  <a href="projeto.php?projeto=<?= $projeto['id'] ?>">
-                     <div class="image_default">
-                        <img class="centrare" style="object-fit: cover; width:225px; height:280px;" src="../backoffice/assets/projetos/<?= $projeto['fotografia'] ?>" alt="">
-                        <div class="imgText justify-content-center m-auto"><?= $projeto['nome'] ?></div>
-                     </div>
-                  </a>
-               </div>
-
-            <?php endforeach; ?>
-
          </div>
+      </div>
+   </form>
+   <!-- End of Search Bar -->
 
+   <div class="row justify-content-center mt-3">
+
+      <?php foreach ($projetos as $projeto) : ?>
+
+      <div class="ml-5 imgList">
+         <a href="projeto.php?projeto=<?= $projeto['id'] ?>">
+            <div class="image_default">
+               <img class="centrare" style="object-fit: cover; width:225px; height:280px;" src="../backoffice/assets/projetos/<?= $projeto['fotografia'] ?>" alt="">
+               <div class="imgText justify-content-center m-auto"><?= $projeto['nome'] ?></div>
+            </div>
+         </a>
       </div>
 
+      <?php endforeach; ?>
+
    </div>
+
+</div>
+
+
 </section>
 
 <!-- end product section -->
