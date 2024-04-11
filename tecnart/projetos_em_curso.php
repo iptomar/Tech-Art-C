0ef2be2bcc70af9531398096971891ef3bb877ed
Tiago Ferreira <101589769+JustOverDrive@users.noreply.github.com>
2024-04-11 16:41:11 +0100

    Revert "T title diff"

--- tecnart/projetos_em_curso.php
+++ tecnart/projetos_em_curso.php
@@ -4,19 +4,8 @@ include 'models/functions.php';
 
 $pdo = pdo_connect_mysql();
 $language = ($_SESSION["lang"] == "en") ? "_en" : "";
-
-// Check if search query is provided
-$search_query = isset($_GET['search']) ? $_GET['search'] : '';
-
-// Prepare the SQL query based on the search query
-$query = "SELECT id, COALESCE(NULLIF(nome{$language}, ''), nome) AS nome, fotografia FROM projetos WHERE concluido=false";
-if (!empty($search_query)) {
-    $query .= " AND (nome LIKE :search_query)";
-    $stmt = $pdo->prepare($query);
-    $stmt->bindValue(':search_query', '%' . $search_query . '%', PDO::PARAM_STR);
-} else {
-    $stmt = $pdo->prepare($query);
-}
+$query = "SELECT id,COALESCE(NULLIF(nome{$language}, ''), nome) AS nome,fotografia FROM projetos WHERE concluido=false";
+$stmt = $pdo->prepare($query);
 $stmt->execute();
 $projetos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
@@ -31,12 +20,15 @@ $projetos = $stmt->fetchAll(PDO::FETCH_ASSOC);
    <div style="background-color: #dbdee1; padding-top: 50px; padding-bottom: 50px;">
       <div class="container">
          <div class="heading_container3">
+
             <h3 style="margin-bottom: 5px;">
                <?= change_lang("projects-ongoing-page-heading") ?>
             </h3>
+
             <h5 class="heading2_h5">
                <?= change_lang("projects-ongoing-page-description") ?>
             </h5>
+
          </div>
       </div>
    </div>
@@ -44,25 +36,12 @@ $projetos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <!-- end product section -->
 
 <section class="product_section layout_padding">
-   <!-- Search Bar -->
-   <form method="GET" action="projetos_em_curso">
-      <div class="row justify-content-center">
-         <div class="col-md-6 mb-3">
-            <div class="input-group mb-3">
-               <input type="text" name="search" class="form-control" placeholder="Search projects..." id="searchInput">
-               <div class="input-group-append">
-                  <button class="btn btn-outline-secondary" type="submit" id="searchButton"><i class="fa fa-search"></i></button>
-               </div>
-            </div>
-         </div>
-      </div>
-   </form>
-   <!-- End of Search Bar -->
-
    <div style="padding-top: 20px;">
       <div class="container">
          <div class="row justify-content-center mt-3">
+
             <?php foreach ($projetos as $projeto) : ?>
+
                <div class="ml-5 imgList">
                   <a href="projeto.php?projeto=<?= $projeto['id'] ?>">
                      <div class="image_default">
@@ -71,9 +50,12 @@ $projetos = $stmt->fetchAll(PDO::FETCH_ASSOC);
                      </div>
                   </a>
                </div>
+
             <?php endforeach; ?>
+
          </div>
       </div>
+
    </div>
 </section>
 
@@ -82,4 +64,5 @@ $projetos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <?= template_footer(); ?>
 
 </body>
+
 </html>
