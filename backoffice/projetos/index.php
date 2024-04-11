2bd94bc2f7e1804ce0a6f80539d780086ae30764
DiniSilva <101589841+DiniSilva@users.noreply.github.com>
2024-04-11 17:25:10 +0100

    links do icon de pesquisa adicionado

--- backoffice/projetos/index.php
+++ backoffice/projetos/index.php
@@ -25,9 +25,10 @@ $sql2 = "SELECT p.id, p.nome, p.referencia, p.areapreferencial, p.financiamento,
 $result_gestores = mysqli_query($conn, $sql2);
 
 ?>
-
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Varela+Round">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
