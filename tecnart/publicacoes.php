11a59db1c00aa914620afc7d58a4b5276d8ce55f
Tiago Ferreira <101589769+JustOverDrive@users.noreply.github.com>
2024-04-18 18:04:42 +0100

    Update publicacoes.php
    Feito filtro por ano

--- tecnart/publicacoes.php
+++ tecnart/publicacoes.php
@@ -2,6 +2,24 @@
 include 'config/dbconnection.php';
 include 'models/functions.php';
 
+// Função para retornar todos os anos distintos de publicações, ordenados da mais recente para a mais antiga
+function getDistinctYears($pdo)
+{
+    $query = "SELECT DISTINCT YEAR(data) AS publication_year FROM publicacoes ORDER BY publication_year DESC";
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+// Função para verificar se um ano está selecionado para filtragem
+function isYearSelected($year)
+{
+    if (isset($_GET['years']) && in_array($year, $_GET['years'])) {
+        return true;
+    }
+    return false;
+}
+
 ?>
 
 <?= template_header('Publicações'); ?>
@@ -12,22 +30,53 @@ include 'models/functions.php';
                 <h3 class="heading_h3" style="text-transform: uppercase;">
                     <?= change_lang("publications-page-heading") ?>
                 </h3><br><br>
+
+                <!-- Formulário para filtrar por ano de publicações -->
+                <div class="text-left">
+                    <form id="filterForm" method="get" action="<?= $_SERVER['PHP_SELF']; ?>">
+                        <div class="form-group row">
+                            <label for="years" class="col-auto col-form-label">Filtrar Ano:</label>
+                            <div class="col-sm-10">
+                                <select name="years[]" id="years" multiple onchange="submitForm()" class="form-control">
+                                    <option value="">Sem Filtros</option>
+                                    <?php
+                                    $pdo = pdo_connect_mysql();
+                                    $distinctYears = getDistinctYears($pdo);
+                                    foreach ($distinctYears as $year) {
+                                        // Formatando o ano (exibindo quatro dígitos)
+                                        $formattedYear = $year['publication_year'];
+                                        // Verificando se o ano está selecionado
+                                        $selected = isYearSelected($year['publication_year']) ? 'selected' : '';
+                                        echo "<option value='{$year['publication_year']}' $selected>{$formattedYear}</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
                 <?php
-                $pdo = pdo_connect_mysql();
-                if (!isset($_SESSION["lang"])) {
-                    $lang = "pt";
-                } else {
-                    $lang = $_SESSION["lang"];
+                // Filtrar publicações com base nos anos selecionados
+                $whereClause = null; // Definir a cláusula de filtro como nula por padrão
+                if (isset($_GET['years'])) {
+                    if ($_GET['years'][0] !== '') {
+                        $years = implode(",", $_GET['years']);
+                        $whereClause = "AND YEAR(data) IN ($years)";
+                    }
                 }
+
+                $lang = isset($_SESSION["lang"]) ? $_SESSION["lang"] : "pt";
                 $valorSiteName = "valor_site_$lang";
                 $query = "SELECT dados, YEAR(data) AS publication_year, p.tipo, pt.$valorSiteName FROM publicacoes p
                                 LEFT JOIN publicacoes_tipos pt ON p.tipo = pt.valor_API
-                                WHERE visivel = true
+                                WHERE visivel = true $whereClause
                                 ORDER BY publication_year DESC, pt.$valorSiteName, data DESC";
                 $stmt = $pdo->prepare($query);
                 $stmt->execute();
                 $publicacoes = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+                // Agrupar publicações por ano e site
                 $groupedPublicacoes = array();
                 foreach ($publicacoes as $publicacao) {
                     $year = $publicacao['publication_year'];
@@ -48,9 +97,15 @@ include 'models/functions.php';
                     $groupedPublicacoes[$year][$site][] = $publicacao['dados'];
                 }
                 ?>
+
                 <script src="../backoffice/assets/js/citation-js-0.6.8.js"></script>
                 <script>
                     const Cite = require('citation-js');
+
+                    // Função para enviar o formulário automaticamente quando uma opção de ano é selecionada
+                    function submitForm() {
+                        document.getElementById("filterForm").submit();
+                    }
                 </script>
 
                 <div id="publications">
@@ -84,3 +139,5 @@ include 'models/functions.php';
 </section>
 
 <?= template_footer(); ?>
+
+
