fde7b975fe74e35fd22bce1cd88643a99d24e365
DiniSilva <101589841+DiniSilva@users.noreply.github.com>
2024-05-24 15:54:46 +0100

    Update da geração dos relatorios
    Foi feito uma atualização na geração dos relatorios, pois usava uma ferramenta que foi abandonada. logo foi atualizado

--- libraries/vendor/composer/autoload_real.php
+++ libraries/vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInitfe43a9502b282986cdbf89c8e171cbb8
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitfe43a9502b282986cdbf89c8e171cbb8', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitfe43a9502b282986cdbf89c8e171cbb8', 'loadClassLoader'));
--- libraries/vendor/composer/autoload_static.php
+++ libraries/vendor/composer/autoload_static.php
@@ -6,13 +6,57 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitfe43a9502b282986cdbf89c8e171cbb8
 {
-    public static $prefixesPsr0 = array (
+    public static $prefixLengthsPsr4 = array (
+        'Z' => 
+        array (
+            'ZipStream\\' => 10,
+        ),
         'P' => 
         array (
-            'PHPExcel' => 
-            array (
-                0 => __DIR__ . '/..' . '/phpoffice/phpexcel/Classes',
-            ),
+            'Psr\\SimpleCache\\' => 16,
+            'Psr\\Http\\Message\\' => 17,
+            'Psr\\Http\\Client\\' => 16,
+            'PhpOffice\\PhpSpreadsheet\\' => 25,
+        ),
+        'M' => 
+        array (
+            'Matrix\\' => 7,
+        ),
+        'C' => 
+        array (
+            'Complex\\' => 8,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'ZipStream\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/maennchen/zipstream-php/src',
+        ),
+        'Psr\\SimpleCache\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/simple-cache/src',
+        ),
+        'Psr\\Http\\Message\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-factory/src',
+            1 => __DIR__ . '/..' . '/psr/http-message/src',
+        ),
+        'Psr\\Http\\Client\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/psr/http-client/src',
+        ),
+        'PhpOffice\\PhpSpreadsheet\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet',
+        ),
+        'Matrix\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/markbaker/matrix/classes/src',
+        ),
+        'Complex\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/markbaker/complex/classes/src',
         ),
     );
 
@@ -23,7 +67,8 @@ class ComposerStaticInitfe43a9502b282986cdbf89c8e171cbb8
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixesPsr0 = ComposerStaticInitfe43a9502b282986cdbf89c8e171cbb8::$prefixesPsr0;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitfe43a9502b282986cdbf89c8e171cbb8::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitfe43a9502b282986cdbf89c8e171cbb8::$prefixDirsPsr4;
             $loader->classMap = ComposerStaticInitfe43a9502b282986cdbf89c8e171cbb8::$classMap;
 
         }, null, ClassLoader::class);
